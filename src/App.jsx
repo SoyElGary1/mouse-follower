3f527fcdfe7802975d996b3e8122785e460a5fdf
Christopher Nunez <elgary1129@gmail.com>
2024-04-10 09:26:13 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useEffect } from "react"
+
+function App() {
+  useEffect(() => {
+    console.log('efecto')
+  })
+ 
+
+  return (
+    <>
+      <h3>Mouse Follower</h3>
+    </>
+  )
+}
+
+export default App
